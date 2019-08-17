77083b8bcbb9ea4d713a95045cd7a013bfe7cb5d
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2019-08-18 00:03:22 +0700

    Add article item layout and view holder

---
+++ app/src/main/java/com/lukma/hcapplication/presentation/main/ArticleHolder.kt
@@ -0,0 +1,51 @@
+package com.lukma.hcapplication.presentation.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.lukma.hcapplication.R
+import com.lukma.hcapplication.domain.content.Article
+import com.lukma.hcapplication.presentation.common.ImageRequestListener
+import com.lukma.hcapplication.presentation.module.GlideApp
+import kotlinx.android.synthetic.main.article_item.view.*
+
+class ArticleHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var item: Article? = null
+
+    private val thumbnailImageView = itemView.thumbnailImageView
+    private val progressBar = itemView.progressBar
+    private val titleTextView = itemView.titleTextView
+
+    private val options = RequestOptions()
+        .placeholder(R.color.colorAccent)
+        .transform(CenterCrop(), RoundedCorners(16))
+
+    private fun onCreate(onItemClicked: (Article) -> Unit) {
+        itemView.setOnClickListener { item?.run(onItemClicked) }
+    }
+
+    fun onBind(item: Article) {
+        this.item = item
+        GlideApp.with(itemView)
+            .load(item.image)
+            .apply(options)
+            .listener(ImageRequestListener(progressBar))
+            .into(thumbnailImageView)
+        titleTextView.text = item.title
+    }
+
+    companion object {
+        fun newInstance(parent: ViewGroup, onItemClicked: (Article) -> Unit) =
+            ArticleHolder(
+                LayoutInflater
+                    .from(parent.context)
+                    .inflate(R.layout.article_item, parent, false)
+            ).apply {
+                onCreate(onItemClicked)
+            }
+    }
+}
