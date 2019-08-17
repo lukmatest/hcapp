de5cae920e22f4d36ef1725f319e838965420b23
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2019-08-18 02:32:49 +0700

    Simplify implementation data

--- app/src/main/java/com/lukma/hcapplication/data/content/ContentDataRepository.kt
+++ app/src/main/java/com/lukma/hcapplication/data/content/ContentDataRepository.kt
@@ -1,11 +1,8 @@
 package com.lukma.hcapplication.data.content
 
 import com.lukma.hcapplication.data.content.cloud.ContentApi
-import com.lukma.hcapplication.domain.content.Article
 import com.lukma.hcapplication.domain.content.ContentRepository
-import com.lukma.hcapplication.domain.content.Product
 
 class ContentDataRepository(private val contentApi: ContentApi) : ContentRepository {
-    override suspend fun getHomeContent(): Pair<List<Product>, List<Article>> =
-        contentApi.getHomeContent().let(::transform)
+    override suspend fun getHomeContent() = contentApi.getHomeContent().let(::transform)
 }
