bc7ba521461738a8747f55f72bb32bb5d092c264
Lukma Gayo Arizky <lukma.gayo.arizky@gmail.com>
2019-08-18 00:26:03 +0700

    Create product list adapter

---
+++ app/src/main/java/com/lukma/hcapplication/presentation/main/ProductListAdapter.kt
@@ -0,0 +1,26 @@
+package com.lukma.hcapplication.presentation.main
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lukma.hcapplication.domain.content.Product
+
+class ProductListAdapter(private val onItemClicked: (Product) -> Unit) :
+    RecyclerView.Adapter<ProductHolder>() {
+
+    private var items = mutableListOf<Product>()
+
+    fun addItems(items: List<Product>) {
+        this.items.clear()
+        this.items.addAll(items)
+        this.notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ProductHolder.newInstance(parent, onItemClicked)
+
+    override fun onBindViewHolder(holder: ProductHolder, position: Int) {
+        holder.onBind(items[position])
+    }
+
+    override fun getItemCount() = items.size
+}
